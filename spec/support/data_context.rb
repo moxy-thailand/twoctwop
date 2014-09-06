8d5b7ce904faa40a65c9b154c658730c57dd586b
Zack Siri <zack@codemy.net>
2014-09-06 16:06:02 +0700

    justify response_data hash [#78314150]

--- spec/support/data_context.rb
+++ spec/support/data_context.rb
@@ -14,26 +14,26 @@ shared_context "data" do
 
   let(:response_data) do 
     { 
-      amt: "000000040000",
-      approvalCode: "747505",
-      dateTime: "060914155526",
-      eci: "07",
-      failReason: "Approved",
-      hashValue: "12C6523EDAF49AE7D3189F98FC88709ACC0BC173",
-      merchantID: "JT01",
-      pan: "411111XXXXXX1111",
-      refNumber: "0906110000041",
-      respCode: "00",
-      status: "A",
-      timeStamp: "060914155630",
-      tranRef: "M1000",
+      amt:                   "000000040000",
+      approvalCode:          "747505",
+      dateTime:              "060914155526",
+      eci:                   "07",
+      failReason:            "Approved",
+      hashValue:             "12C6523EDAF49AE7D3189F98FC88709ACC0BC173",
+      merchantID:            "JT01",
+      pan:                   "411111XXXXXX1111",
+      refNumber:             "0906110000041",
+      respCode:              "00",
+      status:                "A",
+      timeStamp:             "060914155630",
+      tranRef:               "M1000",
       uniqueTransactionCode: "M1000",
-      userDefined1: nil,
-      userDefined2: nil,
-      userDefined3: nil,
-      userDefined4: nil,
-      userDefined5: nil, 
-      version: "8.0"
+      userDefined1:          nil,
+      userDefined2:          nil,
+      userDefined3:          nil,
+      userDefined4:          nil,
+      userDefined5:          nil, 
+      version:               "8.0"
     }
   end
 end
