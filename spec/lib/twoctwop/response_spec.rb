987d6dd45af9d0010c38745eafac6a9f480ca994
Zack Siri <zack@codemy.net>
2014-09-06 16:04:13 +0700

    cleaned up test

--- spec/lib/twoctwop/response_spec.rb
+++ spec/lib/twoctwop/response_spec.rb
@@ -7,31 +7,6 @@ module Twoctwop
 
     let(:response_body) { File.read(File.join(Twoctwop.root, 'spec', 'fixtures', 'response_body.txt')) }
 
-    let(:response_data) do 
-      { 
-        amt: "000000040000",
-        approvalCode: "747505",
-        dateTime: "060914155526",
-        eci: "07",
-        failReason: "Approved",
-        hashValue: "12C6523EDAF49AE7D3189F98FC88709ACC0BC173",
-        merchantID: "JT01",
-        pan: "411111XXXXXX1111",
-        refNumber: "0906110000041",
-        respCode: "00",
-        status: "A",
-        timeStamp: "060914155630",
-        tranRef: "M1000",
-        uniqueTransactionCode: "M1000",
-        userDefined1: nil,
-        userDefined2: nil,
-        userDefined3: nil,
-        userDefined4: nil,
-        userDefined5: nil, 
-        version: "8.0"
-      }
-    end
-
     describe "#decrypt_body" do 
       it "should decrypt the body into xml" do 
         response = Twoctwop::Response.new(response_body)
