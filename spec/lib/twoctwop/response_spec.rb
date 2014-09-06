6fb1bc89ef503e69519454eaa852c4c9f7b81db1
Zack Siri <zack@codemy.net>
2014-09-06 16:02:49 +0700

    gem can successfull build and decrypt request / response from 2c2p api [#78314150]

--- spec/lib/twoctwop/response_spec.rb
+++ spec/lib/twoctwop/response_spec.rb
@@ -5,10 +5,37 @@ module Twoctwop
   describe Response do 
     include_context "data"
 
+    let(:response_body) { File.read(File.join(Twoctwop.root, 'spec', 'fixtures', 'response_body.txt')) }
+
+    let(:response_data) do 
+      { 
+        amt: "000000040000",
+        approvalCode: "747505",
+        dateTime: "060914155526",
+        eci: "07",
+        failReason: "Approved",
+        hashValue: "12C6523EDAF49AE7D3189F98FC88709ACC0BC173",
+        merchantID: "JT01",
+        pan: "411111XXXXXX1111",
+        refNumber: "0906110000041",
+        respCode: "00",
+        status: "A",
+        timeStamp: "060914155630",
+        tranRef: "M1000",
+        uniqueTransactionCode: "M1000",
+        userDefined1: nil,
+        userDefined2: nil,
+        userDefined3: nil,
+        userDefined4: nil,
+        userDefined5: nil, 
+        version: "8.0"
+      }
+    end
+
     describe "#decrypt_body" do 
       it "should decrypt the body into xml" do 
-        request = Twoctwop::Request.new(data)
-        request.make_non_3ds_payment!
+        response = Twoctwop::Response.new(response_body)
+        expect(response.decrypt_body).to eq Hashie::Mash.new(response_data)
       end
     end
   end
--- spec/support/data_context.rb
+++ spec/support/data_context.rb
@@ -3,12 +3,12 @@ shared_context "data" do
     {
       uniqueTransactionCode: "M1000",
       desc:                  "Polo Shirt",
-      amt:                   "0000000040000",
-      currencyCode:          764,
+      amt:                   "000000040000",
+      currencyCode:          702,
       panBank:               "Siam Commercial Bank",
-      panCountry:            "TH",
-      cardHolderName:        "Zack Siri",
-      encCardInfo:           "00acjp+7erduPfJ6Wvrhp0L27fXGaue2/MPK/MUZtjnXRzrfCUpvG1c6ALQkogrYhqYVUDs5TGPoadv4nRwb2k5I/N/euS0srxJA0ikI9oC4xlM0NdLzdERgOzJrcsVR+QpVljjH7/lZ+PIMxwVR0X1vw3HRFxDuMiRARS3bxmmug8k=U2FsdGVkX1+HhQz45KZR33KzurxLl7VfYLofayanY+BfnBJJcgOHhK0rjNXqw5Se"
+      panCountry:            "SG",
+      cardHolderName:        "John Doe",
+      encCardData:           "00acjp+7erduPfJ6Wvrhp0L27fXGaue2/MPK/MUZtjnXRzrfCUpvG1c6ALQkogrYhqYVUDs5TGPoadv4nRwb2k5I/N/euS0srxJA0ikI9oC4xlM0NdLzdERgOzJrcsVR+QpVljjH7/lZ+PIMxwVR0X1vw3HRFxDuMiRARS3bxmmug8k=U2FsdGVkX1+HhQz45KZR33KzurxLl7VfYLofayanY+BfnBJJcgOHhK0rjNXqw5Se"
     }
   end
 end
