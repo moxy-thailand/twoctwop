7dcd07c2d6fc1d7466fb55b987ca024908423868
Zack Siri <zack@codemy.net>
2014-09-07 17:52:16 +0700

    Added sample data for charging with token [#78328698]

--- spec/support/data_context.rb
+++ spec/support/data_context.rb
@@ -12,6 +12,20 @@ shared_context "data" do
     }
   end
 
+  let(:token_data) do 
+    {
+      uniqueTransactionCode: "M1000",
+      desc:                  "Polo Shirt",
+      amt:                   "000000040000",
+      currencyCode:          702,
+      panBank:               "Siam Commercial Bank",
+      panCountry:            "SG",
+      cardHolderName:        "John Doe",
+      encCardData:           "00acjp+7erduPfJ6Wvrhp0L27fXGaue2/MPK/MUZtjnXRzrfCUpvG1c6ALQkogrYhqYVUDs5TGPoadv4nRwb2k5I/N/euS0srxJA0ikI9oC4xlM0NdLzdERgOzJrcsVR+QpVljjH7/lZ+PIMxwVR0X1vw3HRFxDuMiRARS3bxmmug8k=U2FsdGVkX1+HhQz45KZR33KzurxLl7VfYLofayanY+BfnBJJcgOHhK0rjNXqw5Se",
+      storeCardUniqueID:     "blah" # card token goes here
+    }
+  end
+
   let(:response_data) do 
     { 
       amt:                   "000000040000",
